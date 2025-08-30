8f16e0c511554d68ea9041ad33ed9f64901a0ad7
Jan Lindblad <jan.lindblad@for.eco>
2025-08-30 10:42:19 +0200

    Added some ring related info to home view.

--- home/home_view.py
+++ home/home_view.py
@@ -1,9 +1,19 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 from core.models import Organization, Event
+from ring.models import Ring, RingKey
 
+@login_required
 def home_view(request):
     if not request.user.is_authenticated:
         return render(request, "home/home.html", {"events": None})
     organizations = Organization.objects.filter(stakeholders__user=request.user).distinct()
     events = Event.objects.filter(organizers__in=organizations).distinct()
-    return render(request, "home/home.html", {"events": events})
+
+    # Get rings where user is a member
+    user_rings = Ring.objects.filter(ringkey__user=request.user).distinct()
+    
+    return render(request, 'home/home.html', {
+        'events': events,
+        'user_rings': user_rings
+    })
